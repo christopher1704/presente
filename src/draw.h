ca4bb0484838435d16e3fc02fa75517ded69a420
christopher1704 <33550603+christopher1704@users.noreply.github.com>
2020-05-27 21:52:34 -0400

    Update draw.h

--- src/draw.h
+++ src/draw.h
@@ -3,11 +3,15 @@
 
 #include "level.h"
 #include "state.h"
+#include <raylib.h>
+#include <stdio.h>
 
+#define FRAME_SPEED 8
 // Draws the cells of the level in the screen
 void draw_level(const level *lvl);
 
 // Draws the whole state and the level in the screen
-void draw_state(const level *lvl, const state *sta);
+//void draw_state(const level *lvl, const state *sta);
+void draw_state(const level *lvl,const state *sta, Texture2D player_sprite, Texture2D brute_sprite, Texture2D minion_sprite, Rectangle frame_player, Rectangle *frame_brute, Rectangle *frame_minion, int *frame_actual_enemigo, int *frame_cont_enemigo);
 
 #endif
